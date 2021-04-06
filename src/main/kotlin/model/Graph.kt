6db8e56fac1f6f92a3ab3cd8b141b2a4033bd529
Gashmob <kevin@ktraini.com>
2021-04-06 16:23:40 +0200

    Add CRUD for Node

--- src/main/kotlin/model/Graph.kt
+++ src/main/kotlin/model/Graph.kt
@@ -2,6 +2,7 @@ package model
 
 import logger.info
 import logger.success
+import logger.warning
 import model.enums.NodeType
 
 class Graph(private val nodes: ArrayList<Node>, private val relations: ArrayList<Relation>, private var index: Int) {
@@ -34,7 +35,7 @@ class Graph(private val nodes: ArrayList<Node>, private val relations: ArrayList
     ): Node? {
         var off = offset
 
-        nodes.forEach { it ->
+        nodes.forEach {
             // Test simple parameters
             if ((if (name == null) true else it.name == name) &&
                 (if (uri == null) true else it.uri == uri) &&
@@ -51,6 +52,7 @@ class Graph(private val nodes: ArrayList<Node>, private val relations: ArrayList
                         }
 
                         if (ok && off-- == 0) {
+                            success("Node found")
                             return it
                         }
                     }
@@ -59,6 +61,81 @@ class Graph(private val nodes: ArrayList<Node>, private val relations: ArrayList
             }
         }
 
+        warning("Node not found")
         return null
+    }
+
+    fun getNodes(
+        name: String? = null,
+        uri: String? = null,
+        type: NodeType? = null,
+        attributes: HashMap<String, Any?>? = null,
+        id: Int? = null,
+        offset: Int = 0,
+        limit: Int = 25
+    ): ArrayList<Node> {
+        val res = ArrayList<Node>()
+
+        for (i in offset until (offset+limit)) {
+            getNode(name, uri, type, attributes, id, offset)?.let { res.add(it) }
+        }
+        success(res.size, " node found")
+
+        return res
+    }
+
+    fun updateNode(
+        name: String? = null,
+        uri: String? = null,
+        attributes: HashMap<String, Any?>? = null,
+        id: Int
+    ): Boolean {
+        val n = getNode(id = id)
+
+        if (n != null) {
+            if (name != null) n.name = name
+            if (uri != null) n.uri = uri
+            attributes?.forEach { (key, value) ->
+                n.attributes[key] = value
+            }
+            success("Node updated")
+
+            return true
+        }
+
+        return false
+    }
+
+    fun deleteNode(id: Int): Boolean {
+        val n = getNode(id = id)
+
+        if (n != null) {
+            relations.forEach {
+                if (it.first == n) {
+                    n.relations.forEach { it2 ->
+                        if (it2.second == it.second) {
+                            n.relations.remove(it2)
+                        }
+                    }
+                    relations.remove(it)
+                    success("Relation removed")
+                } else if (it.second == n) {
+                    it.first.relations.forEach { it2 ->
+                        if (it2.second == n) {
+                            it.first.relations.remove(it2)
+                        }
+                    }
+                    relations.remove(it)
+                    success("Relation removed")
+                }
+            }
+            nodes.remove(n)
+            success("Node removed")
+
+            return true
+        }
+
+        warning("Node not removed")
+        return false
     }
 }
