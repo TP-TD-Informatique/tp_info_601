362f293787bc1f69234acdcc212f275e2eb6782d
ElRoulioKFC <56266482+ElRoulioKFC@users.noreply.github.com>
2021-04-12 17:18:02 +0200

    interpreter updtae

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import interpreter.query
 import logger.exit
 import logger.init
 import view.MainView
@@ -13,10 +14,15 @@ fun main(args: Array<String>) {
 
     exit()
 */
+    query("create (actor {name  :=   wilson ,  firstname:=peter  ,  age:=18   })")
     //select (a:actor WHERE { a.name == wilson} a.node)
     //select (a:actor WHERE { a.name == wilson} a.name)
+    //select (a:actor,b:actor WHERE { a.name == wilson} a.name)
     //select (a:actor WHERE { a.name == wilson, a.firstname != fred, a.age > 25} a.name)
+    /*query("select (a:actor,b:film WHERE { a.name == wilson,b.name == l'aventure c'est l'aventure} a.name,b.name)")
     query("select (a:actor WHERE { a.name == wilson} a.node)")
     query("select (a:actor WHERE { a.name == wilson} a.name)")
     query("select (a:actor WHERE { a.name == wilson, a.firstname != fred, a.age > 25} a.name)")
+  */
 }
+
