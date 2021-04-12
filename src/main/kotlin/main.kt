7407473c2db0bb0a1312eccae2a3a5b30c93fb86
ElRoulioKFC <56266482+ElRoulioKFC@users.noreply.github.com>
2021-04-12 13:46:50 +0200

    Update

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,11 +5,18 @@ import javax.swing.JFrame
 
 
 fun main(args: Array<String>) {
-    init()
+    /*init()
 
     // Vue GUI principale au lancement
     val homeView: JFrame = MainView("Knowledge Graph")
     homeView.isVisible = true
 
     exit()
+*/
+    //select (a:actor WHERE { a.name == wilson} a.node)
+    //select (a:actor WHERE { a.name == wilson} a.name)
+    //select (a:actor WHERE { a.name == wilson, a.firstname != fred, a.age > 25} a.name)
+    query("select (a:actor WHERE { a.name == wilson} a.node)")
+    query("select (a:actor WHERE { a.name == wilson} a.name)")
+    query("select (a:actor WHERE { a.name == wilson, a.firstname != fred, a.age > 25} a.name)")
 }
