4194044896c727b6dda21770aeaab57eed5efaf8
Gashmob <kevin@ktraini.com>
2021-04-12 17:53:31 +0200

    Add some type for enums

--- src/main/kotlin/model/enums/NodeType.kt
+++ src/main/kotlin/model/enums/NodeType.kt
@@ -3,6 +3,8 @@ package model.enums
 import java.io.Serializable
 
 enum class NodeType : Serializable {
+    ACTOR,
+    MOVIE,
     ;
 
     override fun toString() = name
--- src/main/kotlin/model/enums/RelationType.kt
+++ src/main/kotlin/model/enums/RelationType.kt
@@ -3,6 +3,8 @@ package model.enums
 import java.io.Serializable
 
 enum class RelationType : Serializable {
+    PLAYED_IN,
+    DIRECTOR_OF,
     ;
 
     override fun toString() = name
