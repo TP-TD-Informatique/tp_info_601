1d22e83a47d17d9b5db998915e85f240984c4433
Luca <luca.ciampi@etu.univ-smb.fr>
2021-04-13 12:36:18 +0200

    ligne updated

--- src/main/kotlin/components/MyNodesPanel.java
+++ src/main/kotlin/components/MyNodesPanel.java
@@ -17,17 +17,19 @@ public class MyNodesPanel extends JPanel {
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
-        Border relationBorder = BorderFactory.createMatteBorder(0, 0, 10, 0, Color.BLACK);
-        TitledBorder nodeTitleBorder = BorderFactory.createTitledBorder("Noeud");
+        // useless :
+        // Border relationBorder = BorderFactory.createMatteBorder(0, 0, 10, 0, Color.BLACK);
+        // TitledBorder nodeTitleBorder = BorderFactory.createTitledBorder("Noeud");
 
-        Coordonnees coordsNode1 = new Coordonnees(10,10);
-        Coordonnees coordsNode2 = new Coordonnees(40,40);
+        Coordonnees coordsNode1 = new Coordonnees(10, 10);
+        Coordonnees coordsNode2 = new Coordonnees(200, 200);
         // MyNode = JTextarea
-        MyNode node1 = new MyNode("Tom Cruise", coordsNode1);
+        MyNode node1 = new MyNode("ACTEUR :\nTom Cruise\n1967", coordsNode1);
         // MyRelation = JLabel
         MyRelation rel1 = new MyRelation("PLAYED_IN");
-        rel1.setBorder(relationBorder);
-        MyNode node2 = new MyNode("Top Gun\n1982", coordsNode2);
+        // useless :
+        // rel1.setBorder(relationBorder);
+        MyNode node2 = new MyNode("FILM :\nTop Gun\n1982", coordsNode2);
 
         nodes.add(node1);
         nodes.add(node2);
@@ -45,23 +47,31 @@ public class MyNodesPanel extends JPanel {
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
-        g.setColor(new Color(54,34,56));
+        g.setColor(new Color(54, 34, 56));
         for (int i = 0; i < nodes.size(); ++i) {
             MyNode actualNode = nodes.get(i);
             ArrayList<MyNode> relations = new ArrayList<MyNode>();
             relations = actualNode.getRelations();
             for (int j = 0; j < relations.size(); ++j) {
-                drawLine(actualNode.getCoords(), relations.get(j).getCoords(), g2);
+                drawLine(actualNode, relations.get(j), g2);
             }
         }
     }
 
-    private void drawLine(Coordonnees coordA, Coordonnees coordB, Graphics2D g2) {
-        JTextArea label = new JTextArea("nomRelation");
-        label.setBounds(coordB.getX()-coordA.getX(), coordB.getY()-coordA.getY(), label.getPreferredSize().width, label.getPreferredSize().height);
-        this.add(label);
+    private void drawLine(MyNode node1, MyNode node2, Graphics2D g2) {
+        // Le label qui indique le nom de la liaison
+        JTextField label = new JTextField("nomRelation");
+        int labelXpos = (node2.getCoords().getX() + node1.getCoords().getX()) / 2;
+        int labelYpos = (node2.getCoords().getY() + node1.getCoords().getY()) / 2;
+        label.setBackground(Color.LIGHT_GRAY);
+        label.setBounds(labelXpos, labelYpos, label.getPreferredSize().width, label.getPreferredSize().height);
+
+        // La ligne
         g2.setStroke(new BasicStroke(2));
-        g2.drawLine(coordA.getX(),coordA.getY(),
-                coordB.getX(),coordB.getY());
+        g2.drawLine(node1.getCoords().getX() + (node1.getDimensions().width) / 2,
+                node1.getCoords().getY() + (node1.getDimensions().height) / 2,
+                node2.getCoords().getX(),
+                node2.getCoords().getY());
+        this.add(label);
     }
 }
